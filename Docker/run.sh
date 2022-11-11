fbf3597dcadb774521c87f870132381416dbf0fa
shunpe-hash <d1301539@gmail.com>
2022-11-11 15:17:04 +0900

    fix run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,4 +1,4 @@
 docker run --gpus all --rm -it  \
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
-    -v $HOME/UR:/root/UR \
-    ur-ubuntu18 bash 
+    -v $HOME/ur-Ubuntu18:/root/ur-Ubuntu18 \
+    ur-ubuntu18 bash
