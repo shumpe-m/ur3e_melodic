7b0de8a79074ee916af29732890dcd10e0720c52
shunpe-hash <d1301539@gmail.com>
2022-11-16 15:28:26 +0900

    Add script and update Dockerfile

---
+++ Docker/src/ur_control/scripts/plotting_rpy_2d.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import rospy
+from gazebo_msgs.msg import LinkStates
+
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# https://lilaboc.work/archives/23793422.html
+class PlottingRPY:
+    def __init__(self):
+        ## subscriber
+        self.sub_vector = rospy.Subscriber("/gazebo/link_states", LinkStates, self.callbackVector)
+        ## msg
+        self.rpy = LinkStates()
+        ## list
+        self.list_x = []
+        self.list_y = []
+        self.list_z = []
+        ## line
+        self.line = None
+        ## time
+        # self.start_time = time.time()
+        ## parameter
+        self.interval = 0.1
+        self.shown_size = 1000
+
+        ## initialization
+        self.initializePlot()
+        ## loop
+        self.mainLoop()
+
+    def callbackVector(self, msg):
+        self.rpy = msg
+
+    def initializePlot(self):
+        plt.figure(figsize = [12,4,4])
+        plt.ion()	#interactive mode on
+        ## empty list
+        self.list_x = [0 for i in range(self.shown_size)]
+        self.list_y = [0 for i in range(self.shown_size)]
+        self.list_z = [0 for i in range(self.shown_size)]
+
+        ## xy
+        plt.subplot(3, 1, 1)
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
+        self.line_xy, = plt.plot(self.list_x, self.list_y)
+        ## xz
+        plt.subplot(3, 1, 2)
+        plt.xlabel("x")
+        plt.ylabel("z")
+        plt.xlim(-1, 1)
+        plt.ylim(0, 2)
+        self.line_xz, = plt.plot(self.list_x, self.list_z)
+        ## yz
+        plt.subplot(3, 1, 3)
+        plt.xlabel("y")
+        plt.ylabel("z")
+        plt.xlim(-1, 1)
+        plt.ylim(0, 2)
+        self.line_yz, = plt.plot(self.list_y, self.list_z)
+
+
+    def mainLoop(self):
+        while not rospy.is_shutdown():
+            self.updatePlot()
+            self.drawPlot()
+
+    def updatePlot(self):
+        ## append
+        if self.rpy.pose != []:
+            #print(self.rpy)
+            self.list_x.append(self.rpy.pose[7].position.x)
+            self.list_y.append(self.rpy.pose[7].position.y)
+            self.list_z.append(self.rpy.pose[7].position.z)
+            ## pop
+            self.list_x.pop(0)
+            self.list_y.pop(0)
+            self.list_z.pop(0)
+            ## xy
+            self.line_xy.set_xdata(self.list_x)
+            self.line_xy.set_ydata(self.list_y)
+            ## xz
+            self.line_xz.set_xdata(self.list_x)
+            self.line_xz.set_ydata(self.list_z)
+            ## yz
+            self.line_yz.set_xdata(self.list_y)
+            self.line_yz.set_ydata(self.list_z)
+
+
+
+    def drawPlot(self):
+        ## draw
+        plt.draw()
+        plt.pause(self.interval)
+
+def main():
+    rospy.init_node('plotting_rpy', anonymous=True)
+    plotting_rpy = PlottingRPY()
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # ur-Ubuntu18
+
+## Set up
+```
+git clone https://github.com/shunpe-hash/ur-Ubuntu18.git
+cd ur-Ubuntu18/Docker
+./build.sh
+./run.sh
+```
+
+## Run ur3e
+5つのターミナルでそれぞれコマンドを使用。(docker exec -it ${docker_container_id} bash)
+
+1つめのターミナル:Gazebo起動
+```
+roslaunch ur_gazebo ur3e_bringup.launch
+```
+
+2つめのターミナル:Moveit!ノードを起動
+```
+roslaunch ur3e_moveit_config moveit_planning_execution.launch sim:=true
+```
+
+3つめのターミナル:Moveit!プラグインを含んだRvizを起動
+```
+roslaunch ur3e_moveit_config moveit_rviz.launch
+```
+
+4つめのターミナル:GazeboのUR3eのエンドエフェクタの軌道を描画(過去10sの軌道)
+```
+rosrun ur_control plotting_rpy_2d.py
+```
+
+5つめのターミナル:UR3eの軌道生成コードの起動（y-z空間でエンドエフェクタが円を描く軌道）
+```
+rosrun ur_control ur_planner.py
+```
