b2c5b2ebcca3b1027cf391de917e0f6886067b16
Shumpe Morita <65478897+shumpe-m@users.noreply.github.com>
2022-12-15 17:27:08 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker run --gpus all --rm -it  \
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
-#    -v $HOME/ur3e_melodic:/root/ur3e_melodic \
+    -v $HOME/ur3e_melodic:/root/ur3e_melodic \
     ur3e-melodic bash
