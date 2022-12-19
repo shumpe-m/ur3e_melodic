d44fdab66e0a6398245def047f695a7652add826
shunpe-hash <d1301539@gmail.com>
2022-12-19 18:32:36 +0900

    rename files and folder

--- ur3e_motion/setup.py
+++ ur3e_motion/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['ur_control'],
+    packages=['ur3e_motion'],
     package_dir={'': 'src'}
 )
 
